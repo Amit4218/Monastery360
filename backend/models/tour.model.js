d8ccc3b97c56722ab915982c6efd0d239c6e07e9
Amit4218 <amitbhagat621@gmail.com>
2025-09-24 11:35:36 +0530

    feat:added backend & readme updates

---
+++ backend/models/tour.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const tourSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  tourName: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+  guest: {
+    type: String,
+    required: true,
+  },
+  cab: {
+    type: String,
+    required: true,
+  },
+  guide: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  specialRequests: {
+    type: String,
+    default: "",
+  },
+  totalPrice: {
+    type: String,
+    required: true,
+  },
+});
+
+const TourSchema = mongoose.model("TourSchema", tourSchema);
+export default TourSchema;
